26fd13fbad21e7bfacfb6f318d00f7dc131de2ff
daniel sarabia viera <dani_sv_ctv@hotmail.com>
2017-09-12 18:58:08 +0200

    Added new widget

--- DemoDatabase/dbo/Data/Widgets.sql
+++ DemoDatabase/dbo/Data/Widgets.sql
@@ -2,4 +2,5 @@
 INSERT [dbo].[Widgets] ([ID], [Name]) VALUES (1, N'StackOverlow Questions')
 INSERT [dbo].[Widgets] ([ID], [Name]) VALUES (2, N'StackOverflow Users')
 INSERT [dbo].[Widgets] ([ID], [Name]) VALUES (3, N'GitHub Repositories')
+INSERT [dbo].[Widgets] ([ID], [Name]) VALUES (4, N'Countries')
 SET IDENTITY_INSERT [dbo].[Widgets] OFF
